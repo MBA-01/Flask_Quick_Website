64319fd77fd602fe9c36e8f1ad5058c9890ae68b
MBA-01 <bachriouijr@gmail.com>
2024-01-03 19:52:34 +0100

    adding route to profile (insert username after 'profile/' )

--- views.py
+++ views.py
@@ -4,5 +4,8 @@ views = Blueprint(__name__,"views")
 
 @views.route("/")
 def home():
-    
-    return render_template("index.html",name="Mohamed", age=24) # We can pass variable through here as 'name' and displayed on the html page
+    return render_template("index.html",name="Mohamed") # We can pass variable through here as 'name' and displayed on the html page
+
+@views.route("/profile/<username>")
+def profile(username):
+    return render_template("index.html", name=username)
