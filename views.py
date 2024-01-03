538361c9c0cc5560d42af90fb242afa17437a044
MBA-01 <bachriouijr@gmail.com>
2024-01-03 19:41:25 +0100

    third Commit (Building Home Page)

--- views.py
+++ views.py
@@ -1,12 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 views = Blueprint(__name__,"views")
 
 @views.route("/")
 def home():
-    return "First home page"
-
-
-@views.route("/home")
-def home2():
-    return "Second home page"
+    return render_template("index.html")
