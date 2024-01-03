131c652ff83609fe88c9e2a56db871e66093ef98
MBA-01 <bachriouijr@gmail.com>
2024-01-03 19:47:54 +0100

    Adding var to html file

--- views.py
+++ views.py
@@ -4,4 +4,5 @@ views = Blueprint(__name__,"views")
 
 @views.route("/")
 def home():
-    return render_template("index.html")
+    
+    return render_template("index.html",name="Mohamed", age=24) # We can pass variable through here as 'name' and displayed on the html page
